d474175d0a6626f96fbb58ccd3853693859fea8e
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-18 19:52:43 +0200

    fix jsnack3

--- assets/js/jsnack3.js
+++ assets/js/jsnack3.js
@@ -12,25 +12,29 @@ STRUMENTI:
 //inseriamo tutti i numeri in input, in un array. Quindi definiamolo vuoto.
 let numArray = [];
 
+let sum = 0;
 //chiediamo un numero per 10 volte e lo riportiamo alla relativa posizione dell'array definita dal ciclo for.
 for (let i = 0; i < 10; i++) {
     //chiedo l'input
     let inputNum = parseInt(prompt(`Inserisci un numero. (Te lo chiederò altre ${10 - i} volte)`));
     // lo inserisco nell'array
+    // inputNum = numArray[i];
     numArray[i] = inputNum;
+    sum += inputNum;
 }
-
+console.log(sum);
 //Soluzione 1
-const sum = numArray[0] + numArray[1] + numArray[2] + numArray[3] + numArray[4] + numArray[5] + numArray[6] + numArray[7]+numArray[8]+ numArray[9];
-
-// console.log(sum);
+document.querySelector('body').insertAdjacentHTML('afterbegin', sum);
 
 
 //Soluzione 2
-const sum2 = (previousValue, currentValue) => previousValue + currentValue;
+// const sum2 = (previousValue, currentValue) => previousValue + currentValue;
 
-console.log(numArray.reduce(sum2));
-document.querySelector('body').insertAdjacentHTML('afterbegin', numArray.reduce(sum2));
+// console.log(numArray.reduce(sum2));
+// document.querySelector('body').insertAdjacentHTML('afterbegin', numArray.reduce(sum2));
+
+
+
 
 
 
