b3f1138f50e6815e531af7204ba02f2b80ed574d
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-18 15:27:48 +0200

    setup folders | add html struct | start JSnack 1
    write down logic to execute

---
+++ assets/js/jsnack1.js
@@ -0,0 +1,15 @@
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+/*
+STRUMENTI:
+- prompt
+- if
+- innerHTML | insertAdjacentHTML | append()
+*/
+
+//Richiedere 2 input come numeri.
+
+//Controllo se sono type: numbers
+
+//Se il primo numero è maggiore del secondo, stampalo. Altrimenti stampa il secondo.
+
