6ca24d0f24270bb9a26f7540b1286860d99c892c
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-18 15:46:11 +0200

    JSnack 1 complete

--- assets/js/jsnack1.js
+++ assets/js/jsnack1.js
@@ -8,8 +8,22 @@ STRUMENTI:
 */
 
 //Richiedere 2 input come numeri.
+let num1 = parseInt(prompt('Inserisci un numero'));
+let num2 = parseInt(prompt('Inserisci un secondo numero'));
+// console.log(num1, num2);
 
 //Controllo se sono type: numbers
+/*if (num1 === NaN && num2 === NaN){ //NOT WORKING
+    console.log('Nope');
+    alert('Puoi inserire solo numeri');
+    num1 = parseInt(prompt('Inserisci un numero'));
+    num2 = parseInt(prompt('Inserisci un secondo numero'));
+}*/
 
 //Se il primo numero è maggiore del secondo, stampalo. Altrimenti stampa il secondo.
 
+if (num1 > num2){
+    document.querySelector('body').innerHTML = num1;
+} else {
+    document.querySelector('body').innerHTML = num2;
+}
