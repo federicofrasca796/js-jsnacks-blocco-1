8ef5c87fbcb8ceb9bcbd7fc57b7829ac2afe1f03
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-18 19:27:59 +0200

    complete JSnack 8

--- assets/js/jsnack8.js
+++ assets/js/jsnack8.js
@@ -1,10 +1,25 @@
-// Stampa le potenze di 2 fino a 1000.
+// Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
 /*
 STRUMENTI:
-- array
-- Math.pow
+- prompt
 - for
 - if (?)
 - innerHTML | append() | insertAdjacentHTML
 */
+
+//Chiedo il numero all'utente
+const maxNum = prompt('Digita un numero di 4 cifre');
+
+// console.log(maxNum.charAt(0));
+let sum = 0;
+
+for (let i = 0; i < maxNum.length; i++) {
+    const element = parseInt(maxNum.charAt(i));
+    
+    sum += element;
+    // console.log(element);
+}
+
+console.log(sum);
+document.querySelector('body').innerHTML = sum;
---
+++ assets/js/jsnack9.js
@@ -0,0 +1,10 @@
+// Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+/*
+STRUMENTI:
+- prompt
+- for
+- if (?)
+- innerHTML | append() | insertAdjacentHTML
+*/
+
